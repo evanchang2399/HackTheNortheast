9edf818ac3ca15b3e6e92acf2235280eb3e97af7
lucasoooooo <46410300+lucasoooooo@users.noreply.github.com>
2020-06-06 21:44:59 -0400

    Added Spinner for time preferences

--- app/src/main/java/com/example/tempest/HomeActivity.java
+++ app/src/main/java/com/example/tempest/HomeActivity.java
@@ -12,8 +12,10 @@ import java.util.*;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -36,34 +38,11 @@ import okhttp3.Response;
 
 public class HomeActivity extends AppCompatActivity {
     double userTempLow, userTempHigh;
+    int prefTime;
+    String spinnerInput;
     EditText inputTempLow, inputTempHigh;
     Button submitButton;
-
-    //Evan stuff \/
-    Double latitude;
-    Double longitude;
-    String jResponse;
-    String forecastURL;
-    String pastForecastURL;
-    JSONObject currently;
-    JSONObject hourly;
-    JSONObject daily;
-    Double temp;
-    String humidity;
-    String currentInfo;
-    String[] hourTemps;
-    String[] hourUVs;
-    String[] hourHumidities;
-    String[] hourPrecips;
-    String[] hourWinds;
-    String[] dailyHighs;
-    String[] dailyLows;
-    String[] dailyPrecip;
-    String[] dailyHumidities;
-    String[] dailyHighTimes;
-    String[] dailyLowTimes;
-    JSONObject nextWeek;
-
+    Spinner mySpinner;
     ArrayList<String> jsonString;
     private FusedLocationProviderClient fusedLocationProviderClient;
 
@@ -72,9 +51,20 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Drop Down user input
+        mySpinner = (Spinner) findViewById(R.id.dropDownTime);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.prefered_times, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(adapter);
+
+
+
+        //Temp user input
         inputTempHigh = (EditText) findViewById(R.id.inputTempHigh);
         inputTempLow = (EditText) findViewById(R.id.inputTempLow);
 
+        //Submit button
         submitButton = (Button) findViewById(R.id.submitButton);
         submitButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -82,34 +72,36 @@ public class HomeActivity extends AppCompatActivity {
                 try {
                     userTempLow = Integer.valueOf(inputTempLow.getText().toString());
                     userTempHigh = Integer.valueOf(inputTempHigh.getText().toString());
+                    spinnerInput = mySpinner.getSelectedItem().toString();
+                    convertToInt(); //Converts spinner value to -1,0, or 1
                     moveToScheduleActivity(); //Method at the bottom of all the Evan stuff
-                }catch (NumberFormatException en){
+                    Toast.makeText(HomeActivity.this, String.valueOf(prefTime), Toast.LENGTH_SHORT).show();
+
+                }catch (NumberFormatException en){ //No numbers filled out
                     Toast.makeText(HomeActivity.this, "Please fill out your preferences", Toast.LENGTH_SHORT).show();
                 }
-                catch(Exception e){
+                catch(Exception e){ //Something else went wrong... (:o)
                     Toast.makeText(HomeActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-        //Evan stuff \/
-        forecastURL = "https://api.darksky.net/forecast/45b937115ee9a714334343756da12736/37,-122";
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
-        final TextView jsonTextView;
-        final TextView hourlyTemps;
-        final TextView averages;
-        final TextView weeklyAve;
-        final TextView oldDate;
-        final TextView testBox;
-
-
     }
-
+    //Next Activity
     private void moveToScheduleActivity() {
         Intent intent = new Intent(HomeActivity.this, ScheduleActivity.class);
         intent.putExtra("tempLowKey", userTempLow);
         intent.putExtra("tempHighKey", userTempHigh);
+        intent.putExtra("prefTimeKey", prefTime);
         startActivity(intent);
+    }
+    //Converts input, morning, evening or anytime into -1, 0 or 1... just for funsies
+    private void convertToInt(){
+        if (spinnerInput.equals("Morning")) {
+            prefTime = -1;
+        }else if(spinnerInput.equals("Evening")){
+            prefTime = 1;
+        }else { //Both/anytime
+            prefTime = 0;
+        }
     }
 }
